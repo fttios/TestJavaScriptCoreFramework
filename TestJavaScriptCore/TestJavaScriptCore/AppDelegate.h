4d36ef5e13f8a36e8ea36ccfd7022f691bf83406
fttios <fantantanyx@sina.com>
2018-08-27 13:32:17 +0800

    创建项目

---
+++ TestJavaScriptCore/TestJavaScriptCore/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestJavaScriptCore
+//
+//  Created by tantan fan on 2018/8/27.
+//  Copyright © 2018年 tantan fan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
